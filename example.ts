768f7c23e35cf5fb2b662d26cefce3430cee1b68
Ray Zane <raymondzane@gmail.com>
2018-11-02 17:00:59 -0400

    Add more colors to the example!

--- example.ts
+++ example.ts
@@ -1,3 +1,4 @@
+import chalk from "chalk";
 import { Spinner, SpinnerList } from "./src";
 
 const delay = (n: number) => {
@@ -13,32 +14,34 @@ const run = async () => {
   console.log("\n===>> SpinnerList\n");
   const tasks = SpinnerList.start();
 
-  tasks.set("build:web", "Building web...");
-  tasks.set("build:nginx", "Building nginx...");
-  tasks.set("build:assets", "Building assets...");
+  tasks.set("build:web", chalk`{blue build:} web`);
+  tasks.set("build:nginx", chalk`{blue build:} nginx`);
+  tasks.set("build:assets", chalk`{blue build:} assets`);
 
   await delay(2404);
-  tasks.get("build:nginx").finish("Built nginx");
+  tasks.get("build:nginx").finish(chalk`{green build:} nginx`);
 
   await delay(429);
-  tasks.get("build:assets").finish("Built assets");
+  tasks.get("build:assets").finish(chalk`{green build:} assets`);
 
   await delay(944);
-  tasks.get("build:web").finish("Built web");
+  tasks.get("build:web").finish(chalk`{green build:} web`);
 
-  tasks.set("push:web", "Pushing web...");
-  tasks.set("push:nginx", "Pushing nginx...");
-  tasks.set("push:assets", "Pushing assets...");
+  tasks.set("push:web", chalk`{blue push:} web`);
+  tasks.set("push:nginx", chalk`{blue push:} nginx`);
+  tasks.set("push:assets", chalk`{blue push:} assets`);
 
   await delay(999);
-  tasks.get("push:assets").finish("Pushed assets");
+  tasks.get("push:assets").finish(chalk`{green push:} assets`);
 
   await delay(2456);
-  tasks.get("push:web").finish("Pushed web");
-  tasks.get("push:nginx").finish("Pushed nginx")
+  tasks.get("push:web").finish(chalk`{red push:} web`);
+  tasks.get("push:nginx").finish(chalk`{red push:} nginx`);
 
   await delay(300);
   tasks.finish();
+
+  console.log();
 };
 
 run().catch(err => {
